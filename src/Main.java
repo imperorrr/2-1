411e39b6110ee1d606f2c78bd415d844f3c266aa
Pёtr <150296585+imperorrr@users.noreply.github.com>
2024-01-24 00:36:56 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
-}
+public class Main {
+    public static void main(String[] args) {
+        int initialBalance = 800;
+        int addend = 5500;
+
+        int bonus = 0;
+        if (addend > 1000) {
+            bonus = addend / 100;
+        }
+        int finalBalance = initialBalance + addend + bonus;
+        System.out.println("Итотовый счёт: " + finalBalance);
+        System.out.println("Бонус: " + bonus);
+
